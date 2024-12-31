26e028795c920f754134776036bb0f5b0a49af15
Arsh <arshbhamla@gmail.com>
2024-12-31 15:30:40 -0500

    More minor corrections made to determine right approximate lux value

--- Code/lightIntensityProject.cpp
+++ Code/lightIntensityProject.cpp
@@ -21,30 +21,27 @@ void setup() {
 
 
 float voltageConversion(int input){
-  //Converts from the 0-1023 scale into the true voltage (mV)
-  return input * 5.0 / 1023;
+  //Converts from the 0-1023 scale into the true voltage
+  return input * (5.0 / 1023);
 }
 
 float resistorConversion(float voltage){
   //Voltage division
   int fixedResistance = 10000; //From sensor datasheet, in Ohms
-  float fixedVoltage = 5000; //In milivolts
-  return fixedResistance * voltage / (fixedVoltage - voltage);
+  float fixedVoltage = 5; //In milivolts
+  return (fixedResistance * voltage) / (fixedVoltage - voltage);
 }
 
 float luxConversion(float resistance){
-  //500 is an estimated constant between light intensity and resistance
-  return 500 / resistance;
+  //50000 is an estimated constant between light intensity and resistance
+  return 50000 / resistance;
 }
 
 void loop() {
   int twoHours = 7200 + 60; //7200 is two hours, 60 seconds for maximum time of setup
   if(time < twoHours){
-    //Serial.println(analogRead(analogPin));
     voltage = voltageConversion((int) analogRead(analogPin));
-    //Serial.println("Voltage: " + (String)voltage);
     resistance = resistorConversion(voltage);
-    //Serial.println("Resistance: " + (String)resistance);
     lightIntensity = luxConversion(resistance);
     Serial.println(lightIntensity);
     digitalWrite(readPin, HIGH);
