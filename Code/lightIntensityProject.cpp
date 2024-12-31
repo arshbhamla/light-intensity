ab9db037f29ddf413fc792b6dc1aa7175a1922a7
Arsh <arshbhamla@gmail.com>
2024-12-31 12:14:39 -0500

    Made corrections to the code and the math logic

--- Code/lightIntensityProject.cpp
+++ Code/lightIntensityProject.cpp
@@ -6,7 +6,8 @@ File Use: Receives data from photoresistor and calculates light intensity.
 
 #include <Arduino.h>
 
-const int analogPin = A5;
+const int analogPin = A2;
+const int readPin = 9;
 int time = 0;
 float voltage;
 float resistance;
@@ -14,17 +15,18 @@ float lightIntensity;
 
 void setup() {
   pinMode(analogPin, INPUT);
+  pinMode(readPin, OUTPUT);
   Serial.begin(9600);
 }
 
 
 float voltageConversion(int input){
   //Converts from the 0-1023 scale into the true voltage (mV)
-  return input * 5.0 * 1000 / 1023;
+  return input * 5.0 / 1023;
 }
 
 float resistorConversion(float voltage){
-  //Derived from the voltage divider rule
+  //Voltage division
   int fixedResistance = 10000; //From sensor datasheet, in Ohms
   float fixedVoltage = 5000; //In milivolts
   return fixedResistance * voltage / (fixedVoltage - voltage);
@@ -36,15 +38,23 @@ float luxConversion(float resistance){
 }
 
 void loop() {
-  int twoHours = 7200 + 20; //7200 is two hours, 20 seconds for maximum time of setup
+  int twoHours = 7200 + 60; //7200 is two hours, 60 seconds for maximum time of setup
   if(time < twoHours){
-    voltage = voltageConversion( (int) analogRead(analogPin) );
+    //Serial.println(analogRead(analogPin));
+    voltage = voltageConversion((int) analogRead(analogPin));
+    //Serial.println("Voltage: " + (String)voltage);
     resistance = resistorConversion(voltage);
+    //Serial.println("Resistance: " + (String)resistance);
     lightIntensity = luxConversion(resistance);
     Serial.println(lightIntensity);
+    digitalWrite(readPin, HIGH);
+    delay(100);
+    digitalWrite(readPin, LOW);
+    }
+  else{
+    digitalWrite(readPin, HIGH);
   }
 
-  delay(1000);
+  delay(900); //With the reading delay of 100ms, this is one second
   time++;
 }
-
